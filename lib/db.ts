9430a6937d7c472b768080b21edb9fbdf0b38ff4
akiphysique <pr@akiphysique.com>
2026-03-13 19:43:00 +0900

    Prisma5にダウングレード

--- lib/db.ts
+++ lib/db.ts
@@ -3,8 +3,6 @@ import { PrismaClient } from '@prisma/client'
 // グローバル変数にPrismaClientを保持する（Next.jsの開発時ホットリロードで接続が増えすぎないようにする）
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  log: ['error'],
-})
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
